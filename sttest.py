eb3f077f0142a51bbb7e9c81f9bfaf9a34704f0d
Swaraag <49351868+Swaraag@users.noreply.github.com>
2022-08-25 16:13:57 -0700

    edited streamlit

--- sttest.py
+++ sttest.py
@@ -1,6 +1,29 @@
 import streamlit as st
 import pandas as pd
 st.write(""" # Polling""")
-st.write("Below are some questions")
+questionText = st.write("Below are some questions we'd love for you to answer!")
+
 
 ccYears = st.slider("How many years will it take for Climate Change to be solved?", 0, 100)
+ccDeath = st.radio("Do you think Climate Change will be the end of humanity?", ("Yes", "No"))
+if (ccDeath == "Yes"):
+    ccDate = st.date_input("What's your best guess for the exact date we will die due to Climate Change?")
+elif (ccDeath == "No"):
+    ccDate = st.date_input("What's your best guess for the exact date we will stop Climate Change?")
+
+csv_data = st.file_uploader("Upload any CSV files you would like to visualize", type='csv')
+if csv_data is not None:
+    df = pd.read_csv(csv_data)
+    try:
+        st.bar_chart(df)
+    except:
+        st.write("Uh oh! Something went wrong!")
+    
+
+if st.button("Submit"):
+    questionText.empty()
+    ccYears.empty()
+    ccDeath.empty()
+    ccDate.empty()
+    csv_data = st.empty()
+    completed = st.write("Thanks for filling out the questions!")
