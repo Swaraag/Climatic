5c0ddac8a0188779d7154a2d1eef419424ac563b
Swaraag <49351868+Swaraag@users.noreply.github.com>
2022-08-25 13:50:05 -0700

    test

--- sttest.py
+++ sttest.py
@@ -2,4 +2,18 @@ import streamlit as st
 import pandas as pd
 st.write(""" # My first app""")
 
+
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+
+import os
+
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
+
 number = st.slider("Pick a number", 0, 100)
